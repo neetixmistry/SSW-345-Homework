9af02db3f16202eb29554669679fe88f4e5ab869
neetixmistry <144962461+neetixmistry@users.noreply.github.com>
2023-12-09 19:31:03 -0500

    Delete SingletonPatternDemo.java

--- SingletonPatternDemo.java
+++
@@ -1,8 +0,0 @@
-import Part1.SingleObject;
-
-public class SingletonPatternDemo {
-    public static void main(String[] args) {
-        SingleObject object = SingleObject.getInstance();
-        object.showMessage();
-    }
-}
