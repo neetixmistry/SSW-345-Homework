4977f15affc457f5bde4eeaa53b1e1390552b1fd
neetixmistry <144962461+neetixmistry@users.noreply.github.com>
2023-12-09 19:09:59 -0500

    Add files via upload

---
+++ SingletonPatternDemo.java
@@ -0,0 +1,8 @@
+import Part1.SingleObject;
+
+public class SingletonPatternDemo {
+    public static void main(String[] args) {
+        SingleObject object = SingleObject.getInstance();
+        object.showMessage();
+    }
+}
